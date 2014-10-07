dca934eb49b7bf9b5656cce8842a5a123306853f
elliot woods <elliot@kimchiandchips.com>
2014-10-07 15:14:24 +0900

    title supports multiline

--- src/ofxCvGui/Widgets/Title.cpp
+++ src/ofxCvGui/Widgets/Title.cpp
@@ -8,22 +8,31 @@ namespace ofxCvGui {
 		level(level) {
 			this->setCaption(caption);
 			this->setBounds(ofRectangle(5, 0, 100, 40));
-			this->onDraw += [this] (ofxCvGui::DrawArguments & args) {
-				int fontSize = 14;
-				switch (this->level) {
-				case H1:
-					fontSize = 24;
-					break;
-				case H2:
-					fontSize = 18;
-					break;
-				case H3:
-					fontSize = 14;
-					break;
-				}
-				auto font = ofxAssets::font(ofxCvGui::defaultTypeface, fontSize);
+			
+			//fix font size
+			int fontSize = 14;
+			switch (this->level) {
+			case H1:
+				fontSize = 24;
+				break;
+			case H2:
+				fontSize = 18;
+				break;
+			case H3:
+				fontSize = 14;
+				break;
+			}
 
-				font.drawString(this->caption, 0, (int) this->getHeight() - 10);
+			//check if we need to increase height
+			auto font = ofxAssets::font(ofxCvGui::defaultTypeface, fontSize);
+			auto textBounds = font.getStringBoundingBox(this->caption, 0, 30);
+			const auto textBottom = textBounds.getBottom();
+			if (textBottom + 10 > this->getHeight()) {
+				this->setHeight(textBottom + 10);
+			}
+			this->onDraw += [this, fontSize] (ofxCvGui::DrawArguments & args) {
+				auto font = ofxAssets::font(ofxCvGui::defaultTypeface, fontSize);
+				font.drawString(this->caption, 0, 30);
 			};
 		}
 	}
