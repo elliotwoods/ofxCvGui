b0caa44070657b730c65b75ea3e29901ead01f86
elliot woods <elliot@kimchiandchips.com>
2014-11-16 17:28:07 +0900

    switch examples

---
+++ Example_ElementTree/src/ofApp.cpp
@@ -0,0 +1,101 @@
+#include "ofApp.h"
+
+using namespace ofxCvGui;
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	ofSetCircleResolution(50);
+	ofEnableSmoothing();
+
+	this->gui.init();
+
+	auto drawPanel = this->gui.addBlank();
+	auto scrollPanel = this->gui.addScroll();
+
+	for(int i=0; i<5; i++) {
+		this->x[i].set("X", ofRandomuf() * 512.0f, 0.0f, 512.0f);
+		this->y[i].set("Y", ofRandomuf() * 512.0f, 0.0f, 512.0f);
+		this->radius[i].set("Radius", ofRandomuf() * 100.0f, 0.0f, 100.0f);
+		this->luminance[i].set("Luminance", floor(ofRandomuf() * 255.0f), 0.0f, 255.0f);
+		this->fill[i].set("Fill", false);
+
+		scrollPanel->add(ElementPtr(new Widgets::Slider(this->x[i])));
+		scrollPanel->add(ElementPtr(new Widgets::Slider(this->y[i])));
+		scrollPanel->add(ElementPtr(new Widgets::Slider(this->radius[i])));
+		scrollPanel->add(ElementPtr(new Widgets::Toggle(this->fill[i])));
+
+		//special slider with a validator to check for whole values
+		auto luminanceSlider = new Widgets::Slider(this->luminance[i]);
+		luminanceSlider->setValidator([] (float & value) {
+			value = floor(value + 0.5f);
+		});
+		scrollPanel->add(ElementPtr(luminanceSlider));
+
+		scrollPanel->add(ElementPtr(new Widgets::Spacer()));
+	}
+
+	drawPanel->onDraw += [this] (ofxCvGui::DrawArguments &) {
+		for(int i=0; i<5; i++) {
+			ofPushStyle();
+			this->fill[i].get() ? ofFill() : ofNoFill();
+			ofSetColor(this->luminance[i]);
+			ofCircle(this->x[i], this->y[i], this->radius[i]);
+			ofPopStyle();
+		}
+	};
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
--- Example_ElementTree/src/ofApp.h
+++ Example_ElementTree/src/ofApp.h
@@ -1,15 +1,16 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxCvGui.h"
 
-class testApp : public ofBaseApp{
+class ofApp : public ofBaseApp{
 
 	public:
 		void setup();
 		void update();
 		void draw();
 
-		void keyPressed  (int key);
+		void keyPressed(int key);
 		void keyReleased(int key);
 		void mouseMoved(int x, int y );
 		void mouseDragged(int x, int y, int button);
@@ -19,4 +20,11 @@ class testApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
 		
+		ofxCvGui::Builder gui;
+
+		ofParameter<float> radius[5];
+		ofParameter<float> x[5];
+		ofParameter<float> y[5];
+		ofParameter<float> luminance[5];
+		ofParameter<bool> fill[5];
 };
