49dcc96bff7d6dc7af9fdeaf62c60929df1df51f
elliot woods <elliot@kimchiandchips.com>
2014-11-18 16:35:19 +0900

    Introduce ElementSlot (not used it yet)

---
+++ src/ofxCvGui/ElementSlot.h
@@ -0,0 +1,13 @@
+#include "Element.h"
+
+namespace ofxCvGui {
+	class ElementSlot : public Element {
+	public:
+		ElementSlot();
+		void setElement(shared_ptr<Element>);
+		shared_ptr<Element> getElement() const;
+		void clearElement();
+	protected:
+		shared_ptr<Element> element;
+	};
+}
